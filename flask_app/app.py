ee0715034b070a6247e9a11073cb2b2d333bf7bb
lohseng97 <chewlohseng@live.com>
2022-05-23 19:44:29 +0800

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -100,5 +100,4 @@ def downloadFromFrontend():
 
 
 if(__name__ == "__main__"):
-	app.run(debug = True)
-	# app.run(debug = True, host = '0.0.0.0', port = 8000)
+	app.run(debug = True, host = '0.0.0.0', port = 8000)
