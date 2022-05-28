f09d551642acd8d0cdde3a937041e5b8d7256361
csndylim <54385174+csndylim@users.noreply.github.com>
2022-05-28 13:48:52 +0800

    combined me n js codes

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -121,15 +121,15 @@ const Header = ({
     locations.forEach((location, idx) => {
       if (location.name != event.target.textContent) {
         tempLocations.push(location);
-        } else {
+      } else {
         //   // js help to remove the routes
-          var removeRoute = directionsResponse.splice(idx, 1);
-          setDirectionsResponse(directionsResponse);
+        var removeRoute = directionsResponse.splice(idx, 1);
+        setDirectionsResponse(directionsResponse);
       }
     });
     setLocations(tempLocations);
     setCircleoptions(null);
-   
+
 
     if (tempLocations.length === 0) {
       setAvgcoordinates({ lat: 1.347, lng: 103.79 });
@@ -146,8 +146,9 @@ const Header = ({
       locations: locations,
     };
     setIsLoading(true)
-     fetch("http://127.0.0.1:5000/test", {
-   // fetch("http://centerpoint.lohseng.com:8000/test", {
+    //  fetch("http://127.0.0.1:5000/test", {
+    // fetch("http://127.0.0.1:8000/test", {
+      fetch("http://centerpoint.lohseng.com:8000/test", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -246,18 +247,28 @@ const Header = ({
     setDirectionsResponse([]);
     for (let i = 0; i <= locations.length - 1; i++) {
       const directionsService = new google.maps.DirectionsService();
-      const test = await directionsService.route({
+      const direction = await directionsService.route({
         origin: locations[i].geometry.location,
         destination: place.geometry.location,
 
         travelMode: google.maps.TravelMode.DRIVING,
       });
 
-      getDirections.push(test);
+      getDirections.push(direction);
+      // let moves = direction.routes.legs.steps
+      let moves = direction.routes[0].legs[0].steps;
+      let distance = direction.routes[0].legs[0].distance.text;
+      let duration = direction.routes[0].legs[0].duration.text;
+      console.log("total distance is : " + distance);
+      console.log("total duration is : " + duration);
+      console.log("moves: ");
+      moves.forEach((move, idx) => {
+        console.log(move.instructions);
+        console.log(move.distance.text);
+        console.log(move.duration.text)
+      });
     }
     setDirectionsResponse(getDirections);
-    console.log("this is get directions:")
-    console.log(getDirections)
 
     // change the selected place's marker to different marker icon
     if (typeof (fixedresults) === "string") {
@@ -363,7 +374,7 @@ const Header = ({
             bg={"white"}
             ml={4} // margin left
             onClick={handleSubmit}
-            isLoading = {isLoading}
+            isLoading={isLoading}
           >
             Submit
           </Button>
