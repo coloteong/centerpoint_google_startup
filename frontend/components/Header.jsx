2330583df2a0f520604cdbe557100197301a3c7b
csndylim <54385174+csndylim@users.noreply.github.com>
2022-05-30 01:00:59 +0800

    added a title and sorry js

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -165,8 +165,8 @@ const Header = ({
     };
     setIsLoading(true)
     //  fetch("http://127.0.0.1:5000/test", {
-    // fetch("http://127.0.0.1:8000/test", {
-      fetch("http://centerpoint.lohseng.com:8000/test", {
+    fetch("http://127.0.0.1:8000/test", {
+      // fetch("http://centerpoint.lohseng.com:8000/test", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-
+import Head from 'next/head'
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -7,8 +7,12 @@ function MyApp({ Component, pageProps }) {
   return (
     // 2. wrap the app with `ChakraProvider`
   <ChakraProvider>
+    <Head>
+    <title>Centerpoint</title>
+    </Head>
+
   <Component {...pageProps} />
-  <title>Centerpoint</title>
+
   </ChakraProvider>
 )
 }
