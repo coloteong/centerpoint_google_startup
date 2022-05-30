b0286ae9f8b841d348ab5ab04f824f113fd3af76
claudia <61492986+coloteong@users.noreply.github.com>
2022-05-30 05:00:11 +0000

    Changed IP address

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -178,9 +178,7 @@ const Header = ({
         locations: locations,
       };
       setIsLoading(true)
-      // fetch("http://127.0.0.1:5000/test", {
-      fetch("http://127.0.0.1:8000/test", {
-        // fetch("http://centerpoint.lohseng.com:8000/test", {
+      fetch("http://centerpoint.lohseng.com:8000/test", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
